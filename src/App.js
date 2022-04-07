ef47434090c3860eff8f2458f1a82879787a8976
MohdZaidFrontend <za752911@gmail.com>
2022-04-07 12:46:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
-   <h1>Know Your Crypto</h1>
+  <>
+  <h1>Zaid is creating a project</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex eos consequuntur, iure ducimus, et praesentium velit ab facilis dolor dolorum dolores, fugiat asperiores earum! Fuga reiciendis accusamus soluta, necessitatibus optio, totam distinctio nostrum asperiores magni ad explicabo nisi natus iure nulla cumque magnam repellat itaque alias. Rerum vero similique nulla!</p>
+  </>
   );
 }
 
